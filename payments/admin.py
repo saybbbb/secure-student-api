c306e29e2f6dda772c3682e00ece1e16add5d5f1
Storm <kjialopier@gmail.com>
2026-05-01 15:41:46 +0800

    feat: create payment model wih encrypted fields

--- payments/admin.py
+++ payments/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from .models import Payment
 
-# Register your models here.
+
+@admin.register(Payment)
+class PaymentAdmin(admin.ModelAdmin):
+    list_display = ('id', 'user', 'cardholder_name', 'amount', 'created_at')
+    list_filter = ('created_at',)
